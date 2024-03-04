aca885d8c800c37628e6b4c7429c1a502bfd80d9
luxejs <luxe.jsx@gmail.com>
2024-03-04 06:05:01 -0500

    debugging

--- src/app/components/cmcWidget/index.tsx
+++ src/app/components/cmcWidget/index.tsx
@@ -1,25 +1,25 @@
-// components/CMCWidget.js
-import { useEffect, useRef } from 'react';
+// // components/CMCWidget.js
+// import { useEffect, useRef } from 'react';
 
-const CMCWidget = () => {
- const widgetRef = useRef(null);
+// const CMCWidget = () => {
+//  const widgetRef = useRef(null);
 
- useEffect(() => {
-    if (!widgetRef.current) return;
+//  useEffect(() => {
+//     if (!widgetRef.current) return;
 
-    const script = document.createElement('script');
-    script.src = 'https://files.coinmarketcap.com/static/widget/coinMarquee.js';
-    script.async = true;
-    widgetRef.current.appendChild(script);
+//     const script = document.createElement('script');
+//     script.src = 'https://files.coinmarketcap.com/static/widget/coinMarquee.js';
+//     script.async = true;
+//     widgetRef.current.appendChild(script);
 
-    return () => {
-      widgetRef.current.removeChild(script);
-    };
- }, []);
+//     return () => {
+//       widgetRef.current.removeChild(script);
+//     };
+//  }, []);
 
- return (
-    <div ref={widgetRef} id="coinmarketcap-widget-marquee" coins="29509,22974,1,1027" currency="USD" theme="dark" transparent="true" show-symbol-logo="true"></div>
- );
-};
+//  return (
+//     <div ref={widgetRef} id="coinmarketcap-widget-marquee" coins="29509,22974,1,1027" currency="USD" theme="dark" transparent="true" show-symbol-logo="true"></div>
+//  );
+// };
 
-export default CMCWidget;
+// export default CMCWidget;
