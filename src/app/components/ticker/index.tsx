6ac54c2e4957e3fbf2c44160366137dcaa939381
luxejs <luxe.jsx@gmail.com>
2024-03-04 06:36:22 -0500

    ts type for ticker

--- src/app/components/ticker/index.tsx
+++ src/app/components/ticker/index.tsx
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link'
 import { SiCoinmarketcap } from "react-icons/si";
 
-
+interface TickerDataInterface {
+    price: string
+    percentChange: string
+}
 
 const Ticker = () => {
-    const [tickerData, setTickerData] = useState(null);
-
+    const [tickerData, setTickerData] = useState<TickerDataInterface | null>(null);
 
     useEffect(() => {
         const fetchPrice = async () => {
@@ -16,9 +18,8 @@ const Ticker = () => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok')
                 }
-                const data = await response.json()
+                const data: TickerDataInterface = await response.json()
                 setTickerData(data)
-
             } catch (error) {
                 console.error('Failed to fetch price:', error)
             }
@@ -29,22 +30,16 @@ const Ticker = () => {
 
     return (
         <Link href="https://coinmarketcap.com/currencies/commune-ai/" target='_blank'>
-
             <div className="stats stats-vertical lg:stats-horizontal shadow">
-
                 <div className="stat">
                     <div className="stat-title flex items-center"><SiCoinmarketcap />COMAI</div>
-
                     <div className="stat-value">{tickerData ? `$${tickerData.price}` : 'Loading...'}</div>
-                    {/* <div className="stat-desc text">{tickerData ? `${tickerData.formattedPercentChange}` : 'Loading...'}</div> */}
                     <div className={`stat-desc ${tickerData ? parseFloat(tickerData.percentChange) < 0 ? 'text-red-600' : 'text-green-600' : null}`}>
                         {tickerData ? `${tickerData.percentChange} (1h)` : 'Loading...'}
                     </div>
                 </div>
-
             </div>
         </Link>
-
     )
 }
 
